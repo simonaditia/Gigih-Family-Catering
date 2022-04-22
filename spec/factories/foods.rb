1d74309d4484e5138b969f05866a6d11aebbe038
simonaditia <smnsitumeang@gmail.com>
2022-04-22 18:47:32 +0700

    Refactor Food spec to use Factories

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :food do
-    name { "MyString" }
-    price { 1.5 }
-    description { "MyText" }
+    name { "Nasi Uduk" }
+    description { "Betawi style steamed rice cooked in coconut milk. Delicious!" }
+    price { 10000.0 }
   end
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,22 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  it 'is valid with a name and a description' do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      price: 15000.0,
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!'
-    )
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:food)).to be_valid
+  end
 
-    expect(food).to be_valid
+  it 'is valid with a name and a description' do
+    expect(FactoryBot.build(:food)).to be_valid
   end
 
   it 'is invalid without a name' do
-    food = Food.new(
-      name: nil,
-      price: 15000.0,
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!'
-    )
+    food = FactoryBot.build(:food, name: nil)
 
     food.valid?
 
@@ -24,17 +18,8 @@ RSpec.describe Food, type: :model do
   end
 
   it "is invalid with a duplicate name" do
-    food1 = Food.create(
-      name: "Nasi Uduk",
-      price: 15000.0,
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!'
-    )
-
-    food2 = Food.new(
-      name: "Nasi Uduk",
-      price: 17000.0,
-      description: "Different description"
-    )
+    food1 = FactoryBot.create(:food, name: 'Nasi Uduk')
+    food2 = FactoryBot.build(:food, name: 'Nasi Uduk')
 
     food2.valid?
 
@@ -42,11 +27,7 @@ RSpec.describe Food, type: :model do
   end
 
   it 'is invalid when price is not numeric' do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      price: 'Halo',
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!'
-    )
+    food = FactoryBot.build(:food, price: 'Halo')
 
     food.valid?
     
@@ -54,22 +35,15 @@ RSpec.describe Food, type: :model do
   end
 
   it 'is invalid when price is below 0.01' do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      price: 0,
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!'
-    )
+    food = FactoryBot.build(:food, price: 0)    
 
     food.valid?
+
     expect(food.errors[:price]).to include("must be greater than or equal to 0.01")
   end
 
   it 'is invalid when description is more than 150 characters' do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      price: 15000.0,
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious! For certain rituals or ceremonies, uduk is usually served with traditional Javanese dishes like kering tempe, urap, and sambel goreng. Humble protein sources, such as, a hard boiled egg, fried tempeh, or fried tofu, can also be included in the package. In todays slametan, modern Indonesian food dish, such as sliced fried egg, telur bumbu Bali, or rendang, may also be included. Some people may also add mie goreng or vermicelli into the dish. Krupuk, rempeyek, or emping can also be added. Each neighbourhood in Jakarta has its own variant of the dish, the most notable being Nasi uduk Slipi from West Jakarta. Kebon Kacang area near Tanah Abang in Central Jakarta is renowned for its nasi uduk. Nasi uduk is a popular dish for the busy commuters in Jakarta, mainly because it is affordable. It can be found throughout the day; some roadside stalls open exclusively in the morning, noon, or night, depending on the demographics of the surrounding area. Stalls near residential areas, marketplaces, train stations, and schools are usually open from morning to noon, while the ones near offices and street-side are usually open afternoon to midnight.'
-    )
+    food = FactoryBot.build(:food, description: "Betawi style steamed rice cooked in coconut milk. Delicious! For certain rituals or ceremonies, uduk is usually served with traditional Javanese dishes like kering tempe, urap, and sambel goreng. Humble protein sources, such as, a hard boiled egg, fried tempeh, or fried tofu, can also be included in the package. In todays slametan, modern Indonesian food dish, such as sliced fried egg, telur bumbu Bali, or rendang, may also be included. Some people may also add mie goreng or vermicelli into the dish. Krupuk, rempeyek, or emping can also be added. Each neighbourhood in Jakarta has its own variant of the dish, the most notable being Nasi uduk Slipi from West Jakarta. Kebon Kacang area near Tanah Abang in Central Jakarta is renowned for its nasi uduk. Nasi uduk is a popular dish for the busy commuters in Jakarta, mainly because it is affordable. It can be found throughout the day; some roadside stalls open exclusively in the morning, noon, or night, depending on the demographics of the surrounding area. Stalls near residential areas, marketplaces, train stations, and schools are usually open from morning to noon, while the ones near offices and street-side are usually open afternoon to midnight.")    
 
     food.valid?
 
