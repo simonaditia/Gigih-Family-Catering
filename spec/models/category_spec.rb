1d9b1c83e17fb7a765ef32605ee75e55b83fd52b
simonaditia <smnsitumeang@gmail.com>
2022-04-22 18:38:26 +0700

    Add validation for Category, invalid with duplicate name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -18,4 +18,18 @@ RSpec.describe Category, type: :model do
 
     expect(category.errors[:name]).to include("can't be blank")
   end
+
+  it "is invalid with a duplicate name" do
+    category1 = Category.create(
+      name: "Fruits"
+    )
+    
+    category2 = Category.new(
+      name: "Fruits"
+    )
+
+    category2.valid?
+    
+    expect(category2.errors[:name]).to include("has already been taken")
+  end
 end
