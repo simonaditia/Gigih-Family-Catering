0df4bb274b8a1af403ff005ac3408bdce77966a8
simonaditia <smnsitumeang@gmail.com>
2022-04-22 18:58:38 +0700

    Refactor Categories_food spec to use Factories

--- spec/models/categories_food_spec.rb
+++ spec/models/categories_food_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesFood, type: :model do
-  it 'is valid with a category_id and a food_id' do
-    category_food = CategoriesFood.new(
-      category_id: 1,
-      food_id: 1
-    )
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:categories_food)).to be_valid
+  end
 
-    expect(category_food).to be_valid
+  it 'is valid with a category_id and a food_id' do
+    expect(FactoryBot.build(:categories_food)).to be_valid
   end
 end
