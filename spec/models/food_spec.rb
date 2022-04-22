cf4abc3a731f1c7904762837f144b8085ae995e7
simonaditia <smnsitumeang@gmail.com>
2022-04-22 14:40:01 +0700

    Add validation for Food, invalid with duplicate name

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -22,4 +22,22 @@ RSpec.describe Food, type: :model do
 
     expect(food.errors[:name]).to include("can't be blank")
   end
+
+  it "is invalid with a duplicate name" do
+    food1 = Food.create(
+      name: "Nasi Uduk",
+      price: 15000.0,
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!'
+    )
+
+    food2 = Food.new(
+      name: "Nasi Uduk",
+      price: 17000.0,
+      description: "Different description"
+    )
+
+    food2.valid?
+
+    expect(food2.errors[:name]).to include("has already been taken")
+  end
 end
