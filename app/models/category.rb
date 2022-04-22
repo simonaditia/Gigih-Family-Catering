ea43070cf96aaa7214e375c719a62905f7898238
simonaditia <smnsitumeang@gmail.com>
2022-04-22 18:25:36 +0700

    Add validation for Category, invalid without name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     has_and_belongs_to_many :foods
+    validates :name, presence: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Category, type: :model do
 
     expect(category).to be_valid
   end
+
+  it 'is invalid without a name' do
+    category = Category.new(
+      name: nil
+    )
+
+    category.valid?
+
+    expect(category.errors[:name]).to include("can't be blank")
+  end
 end
