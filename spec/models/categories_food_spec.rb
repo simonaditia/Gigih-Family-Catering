f4def8cf9ec54d28c1abdfbcfc73048a551e4db4
simonaditia <smnsitumeang@gmail.com>
2022-04-22 18:39:57 +0700

    Add customary spec for CategoriesFoods model

--- spec/models/categories_food_spec.rb
+++ spec/models/categories_food_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a category_id and a food_id' do
+    category_food = CategoriesFood.new(
+      category_id: 1,
+      food_id: 1
+    )
+
+    expect(category_food).to be_valid
+  end
 end
