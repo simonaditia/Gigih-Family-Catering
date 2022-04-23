ba5b517d8fc359f7cc5444264d2d26e82dce16ce
simonaditia <smnsitumeang@gmail.com>
2022-04-24 01:07:24 +0700

    Fixing validation Categories Food

--- app/models/categories_food.rb
+++ app/models/categories_food.rb
@@ -1,4 +1,6 @@
 class CategoriesFood < ApplicationRecord
-    belongs_to :category
-    belongs_to :food
+    belongs_to :category, optional: true
+    belongs_to :food, optional: true
+    validates :category_id, presence: true
+    validates :food_id, presence: true
 end
