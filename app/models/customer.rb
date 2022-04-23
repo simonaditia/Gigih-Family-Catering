ae1964c4b5dd87829fb657ed9ab64e71979b9fcd
simonaditia <smnsitumeang@gmail.com>
2022-04-23 20:25:59 +0700

    Add relationship customer, order, and order_detail

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+    has_many :order
     validates :name, presence: true
     validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
-class Order < ApplicationRecord
+class Order < ApplicationRecorddetails
+    belongs_to :customer
+    has_many :order_details
 end
