761d4ef4878fcd5189ef57f6958575c0fa6d5ce9
simonaditia <smnsitumeang@gmail.com>
2022-04-23 19:17:27 +0700

    db:migrate for Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_23_105821) do
+ActiveRecord::Schema.define(version: 2022_04_23_120716) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 2022_04_23_105821) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "category"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "customer_id"
+    t.string "order_date"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
