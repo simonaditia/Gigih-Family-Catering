c63b6166dfe2785b94634600708d19b3d002acb7
simonaditia <smnsitumeang@gmail.com>
2022-04-24 03:41:50 +0700

    Add rails-controller-testing to Gemfile and add validation for Food controller

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -4,4 +4,10 @@ FactoryBot.define do
     description { "Betawi style steamed rice cooked in coconut milk. Delicious!" }
     price { 10000.0 }
   end
+
+  factory :invalid_food, parent: :food do
+    name { nil }
+    description { nil }
+    price { nil }
+  end
 end
